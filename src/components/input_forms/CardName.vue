2f4cbd82f702e579ae845eb4d2ad81bda710ffe7
Erkand Imeri <erkand.imeri@gmail.com>
2019-11-13 07:22:32 +0100

    Create input_forms folder and start with refactoring

---
+++ src/components/input_forms/CardName.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card-input">
+        <label for="cardName" class="card-input__label">Card Name</label>
+        <input
+          type="text"
+          :id="fields.cardName"
+          v-letter-only
+          @input="changeName"
+          class="card-input__input"
+          :value="formData.cardName"
+          data-card-field
+          autocomplete="off"
+        />
+      </div>
+</template>
+<script>
+export default {
+  name: 'CardName'  
+}
+</script>
+<style lang="css" scoped>
+
+</style>
---
+++ src/components/input_forms/CardNumber.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="card-input">
+        <label for="cardNumber" class="card-input__label">Card Number</label>
+        <input
+          type="tel"
+          :id="fields.cardNumber"
+          @input="changeNumber"
+          @focus="focusCardNumber"
+          @blur="blurCardNumber"
+          class="card-input__input"
+          :value="formData.cardNumber"
+          :maxlength="cardNumberMaxLength"
+          data-card-field
+          autocomplete="off"
+        />
+        <button
+          class="card-input__eye"
+          :class="{ '-active' : !isCardNumberMasked }"
+          title="Show/Hide card number"
+          tabindex="-1"
+          :disabled="formData.cardNumber === ''"
+          @click="toggleMask"
+        ></button>
+      </div>
+</template>
+<script>
+export default {
+  name: 'CardNumber'
+}
+</script>
+<style lang="css" scoped>
+
+</style>
